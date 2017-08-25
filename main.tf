af27e62c42882c4dc0d393104091dd9a3a4c1d04
Andrius Ordojan <aor@siteimprove.dk>
2017-08-25 15:26:26 +0200

    added role, policy

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ provider "aws" {
 resource "aws_security_group" "security-group-ec1-d-vault" {
   name = "security-group-ec1-d-vault"
   description = "security group for vault"
+  vpc_id = "vpc-48267f20"
 
   ingress {
     from_port = 0
@@ -29,13 +30,40 @@ resource "aws_security_group" "security-group-ec1-d-vault" {
   }
 }
 
-missing a role
+resource "aws_iam_instance_profile" "vault-profile" {
+  name  = "vault-profile"
+  role = "${aws_iam_role.ops-vault.name}"
+}
+
+resource "aws_iam_role" "ops-vault" {
+  name = "ops-vault2"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
 
 resource "aws_instance" "vault" {
   ami = "ami-1e339e71"
   instance_type = "t2.micro"
+
   vpc_security_group_ids = ["${aws_security_group.security-group-ec1-d-vault.id}"]
-  
+  subnet_id = "subnet-52628139"
+  iam_instance_profile = "${aws_iam_instance_profile.vault-profile.name}"
+
   tags {
     Name = "vault"
     Owner = "ops"
